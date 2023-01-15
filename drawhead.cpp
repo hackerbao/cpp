d6d98e513e307d960968d7f17df8e2ce45ceb4d9
hackerbao <bao.ky.yu@gmail.com>
2023-01-15 10:00:33 -0800

    drawhead

---
+++ drawhead.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+// print a head, use of functions
+void Head()
+{
+       cout << "  ||||||||||||||||  " << endl;
+       cout << "  |              |  " << endl;
+       cout << "  |    o     o   |  " << endl;
+       cout << " _|              |_ " << endl;
+       cout << "|_                _|" << endl;
+       cout << "  |    |-----|   |  " << endl;
+       cout << "  |              |  " << endl;
+
+}
+
+int main()
+{
+    Head();
+    Head();
+    Head();
+    Head();
+    Head();
+    Head();
+    Head();
+    return 0;
+}
---
+++ hello.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+// traditional first program
+// author: Owen Astrachan, 2/27/99
+
+int main()
+{
+  cout << "Hello Bao" << endl; 
+  return 0;
+}
+
